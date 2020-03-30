27f282113229b85acb4896374c46a39596359ad9
Xpol Wan <xpolife@gmail.com>
2020-03-30 18:25:12 +0800

    Republish to steam.

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = 'Aligned Placement'
 description = 'Align to entities of the same kind when build, plant and deploy'
 author = 'xpolife'
-version = '2.0.0'
+version = '2.0.1'
 
 icon_atlas = "AlignedPlacement.xml"
 icon = "AlignedPlacement.tex"

9f391f2aea1f7357307e886146ad52167d8d5bc8
Xpol Wan <xpolife@gmail.com>
2020-04-11 17:45:09 +0800

    Prepare release of 2.2.0.

--- mod/modinfo.lua
+++ mod/modinfo.lua
@@ -1,7 +1,7 @@
 name = 'Aligned Placement 对齐建造'
 description = 'Help align to already planted/built/deployed same items. 帮助对齐到已经种植、建造、部署的相同物品。支持手柄和鼠标！'
 author = 'xpolife'
-version = '2.1.0'
+version = '2.2.0'
 
 icon_atlas = "AlignedPlacement.xml"
 icon = "AlignedPlacement.tex"
